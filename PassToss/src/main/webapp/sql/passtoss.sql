b94382f4db9e5ad045cf6324a59a655922b6a58d
duawo <duawo@192.168.219.129>
2023-02-01 14:39:19 +0900

    member 테이블 sql문 수정

--- PassToss/src/main/webapp/sql/passtoss.sql
+++ PassToss/src/main/webapp/sql/passtoss.sql
@@ -1,16 +1,19 @@
-create table member(
-	id			varchar2(12) primary key,
-	password	varchar2(10),
-	name		varcha2(15),
-	jumin		varchar2(14),
-	deptno		number(2) references dept(detno),
-	email		varchar2(30),
-	phone		varchar2(15),
-	address		varchar2(150),
-	authority	number(1) check(authoriity in (0,1,2))
-)
+drop table member cascade constraints purge
 
 create table dept(
 	deptno	number(5) primary key,
 	dname	varchar2(20)
 )
+
+create table member(
+	id			varchar2(12) primary key,
+	password	varchar2(10),
+	name		varchar2(15),
+	jumin		varchar2(14),
+	deptno		number(2) references dept(deptno),
+	email		varchar2(30),
+	phone		varchar2(15),
+	address		varchar2(150),
+	authority	number(1) check(authority in (0,1,2))
+)
+
